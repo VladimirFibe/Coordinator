e30e90ba675828c4921d903a3ea059e9ed38ef29
Vladimir Fibe <motiw@icloud.com>
2023-09-21 13:34:14 +0600

    Добавил Swift UI

--- Coordinator/Features/ViewController.swift
+++ Coordinator/Features/ViewController.swift
@@ -1,11 +1,15 @@
-import UIKit
+import SwiftUI
 
 class ViewController: BaseViewController {
+    private lazy var rootView: BridgedView = {
+        Text("Coordinator").bridge()
+    }()
 }
 
 extension ViewController {
     override func setupViews() {
         view.backgroundColor = .green
+        addBridgedViewAsRoot(rootView)
     }
 }
 
